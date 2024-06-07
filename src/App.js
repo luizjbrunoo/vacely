86fc131dff638df46b7f0bab2675461b9849a04c
3dguruai <barbara.villanova@students.iaac.net>
2024-06-07 17:33:45 -0300

    rota dashboard + CORS liberacao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+// import React, { useState } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';
+// import './App.css';
+// import Home from './pages/Home';
+// import './i18n'; // Import the translation setup
+
+// const App = () => {
+//   const [showAuth, setShowAuth] = useState(false);
+
+//   const handleLoginClick = () => {
+//     setShowAuth(true);
+//   };
+
+//   const handleAuthClose = () => {
+//     setShowAuth(false);
+//   };
+
+//   return (
+//     <Authenticator.Provider>
+//       <div className="app-container">
+//         <Router>
+//           <Routes>
+//             <Route path="/" element={<Home onLoginClick={handleLoginClick} />} />
+//           </Routes>
+//         </Router>
+//         {showAuth && (
+//           <div className="auth-overlay">
+//             <Authenticator>
+//               <button onClick={handleAuthClose}>Close</button>
+//             </Authenticator>
+//           </div>
+//         )}
+//       </div>
+//     </Authenticator.Provider>
+//   );
+// };
+
+// export default App;
+
+// src/App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import './App.css';
 import Home from './pages/Home';
-import './i18n'; // Import the translation setup
+import Dashboard from './pages/Dashboard'; // Importa a nova página Dashboard
+import './i18n'; // Importa a configuração de tradução
 
 const App = () => {
   const [showAuth, setShowAuth] = useState(false);
@@ -23,6 +66,7 @@ const App = () => {
         <Router>
           <Routes>
             <Route path="/" element={<Home onLoginClick={handleLoginClick} />} />
+            <Route path="/dashboard" element={<Dashboard />} /> {/* Adiciona a nova rota */}
           </Routes>
         </Router>
         {showAuth && (
@@ -38,3 +82,4 @@ const App = () => {
 };
 
 export default App;
+
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Quicksight from '../Quicksight'; // Importa o componente Quicksight
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <Quicksight />
+    </div>
+  );
+};
+
+export default Dashboard;
